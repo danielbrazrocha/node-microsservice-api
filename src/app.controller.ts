f4aad040a046b03da0a34dfbc1832a0f3f21424f
Daniel Rocha <dgustavo@id.uff.br>
2022-12-12 22:17:33 -0300

    feat: add route to render all notifications

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly appService: AppService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): any {
+    return this.prisma.notification.findMany();
+    // return this.appService.getHello();
   }
 }
