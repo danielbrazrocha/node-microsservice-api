627861248d4504690d97be31a48f233bd3cfd50d
Daniel Rocha <dgustavo@id.uff.br>
2022-12-15 20:02:19 -0300

    feat: implement in-memory-repository methods

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -4,13 +4,29 @@ import { NotificationsRepository } from '../../src/application/repositories/noti
 export class InMemoryNotificationsRepository
   implements NotificationsRepository
 {
-  async findById(notification: string): Promise<Notification | null> {
-    throw new Error('Method not implemented.');
-  }
-  async save(notification: Notification): Promise<void> {
-    throw new Error('Method not implemented.');
-  }
   public notifications: Notification[] = [];
+
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = this.notifications.find(
+      (item) => item.id == notificationId,
+    );
+
+    if (!notification) {
+      return null;
+    }
+
+    return notification;
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const notificationIndex = this.notifications.findIndex(
+      (item) => item.id === notification.id,
+    );
+
+    if (notificationIndex >= 0) {
+      this.notifications[notificationIndex] = notification;
+    }
+  }
 
   async create(notification: Notification) {
     this.notifications.push(notification);
