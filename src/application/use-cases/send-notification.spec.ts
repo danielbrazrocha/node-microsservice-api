548df1edb2556072e08c110a8872d8b46cca0c12
Daniel Rocha <dgustavo@id.uff.br>
2022-12-15 01:21:53 -0300

    feat: add inmemory notifications repository

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,15 +1,21 @@
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
 import { SendNotification } from './send-notification';
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    // Setup
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const sendNotification = new SendNotification(notificationsRepository);
 
+    // Exercise
     const { notification } = await sendNotification.execute({
       category: 'social',
       content: 'THis is a notification',
       recipientId: 'example-recipient-id',
     });
 
-    expect(notification).toBeTruthy();
+    // Verify
+    expect(notificationsRepository.notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications[0]).toEqual(notification);
   });
 });
